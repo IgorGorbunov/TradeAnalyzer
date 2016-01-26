c4cde48923ba32c0384f1f4996ef63070360c3ba
Igor Gorbunov <give@list.ru>
2016-01-26 19:15:44 +0300

    add string functions GetDate, Reverse

--- TradeAnalyzer/StringFunctions.cs
+++ TradeAnalyzer/StringFunctions.cs
@@ -1,16 +1,82 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 
 public static class StringFunctions
 {
+    private const char DateDay = 'd';
+    private const char DateMonth = 'M';
+    private const char DateYear = 'y';
 
     public static string GetClearlyDefinedString(string s)
     {
         return s.Aggregate("", (current, c) => current + GetClearDefinedStringChar(c));
+    }
+
+
+    public static DateTime GetDate(string s, string format)
+    {
+        int day = 1, month = 1;
+        string sYear = "";
+        string rS = Reverse(s);
+        string rFormat = Reverse(format);
+        string predicat = "";
+        bool first = true;
+        int length = format.Length > s.Length
+                             ? s.Length
+                             : format.Length;
+        for (int i = 0; i < length; i++)
+        {
+            if (first)
+            {
+                predicat += rFormat[i];
+                first = false;
+            }
+            else
+            {
+                if (rFormat[i] == predicat[0] && i < length - 1)
+                {
+                    predicat += rFormat[i];
+                }
+                else
+                {
+                    int value = int.Parse(rS.Substring(0, predicat.Length));
+                    switch (predicat[0])
+                    {
+                        case DateDay:
+                            day = value;
+                            break;
+                        case DateMonth:
+                            month = value;
+                            break;
+                        case DateYear:
+                            sYear = rS.Substring(0, predicat.Length);
+                            break;
+                    }
+                    if (predicat.Length > 1)
+                    {
+                        rS = rS.Substring(predicat.Length - 1);
+                    }
+                    predicat = rFormat[i].ToString();
+                }
+            }
+        }
+        if (sYear.Length == 2)
+        {
+            sYear = string.Format("20{0}", sYear);
+        }
+        int year = int.Parse(sYear);
+        return new DateTime(year, month, day);
+    }
+
+    public static string Reverse(string s)
+    {
+        string result = "";
+        for (int i = s.Length - 1; i >= 0; i--)
+        {
+            result += s[i];
+        }
+        return result;
     }
 
     private static string GetClearDefinedStringChar(char c)
