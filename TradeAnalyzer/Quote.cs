cc0224fc0fca4e1feced7c48c537bcf557278d21
Igor Gorbunov <give@list.ru>
2016-01-26 17:44:47 +0300

    add quote class

---
+++ TradeAnalyzer/Quote.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Quote
+{
+    public DateTime Date
+    {
+        get;
+        private set;
+    }
+    public double Open
+    {
+        get;
+        private set;
+    }
+    public double Close
+    {
+        get;
+        private set;
+    }
+    public double High
+    {
+        get;
+        private set;
+    }
+    public double Low
+    {
+        get;
+        private set;
+    }
+    public double Volume
+    {
+        get;
+        private set;
+    }
+
+    public Quote(DateTime dt, double open, double close, double high, double low, double volume)
+    {
+        Date = dt;
+        Open = open;
+        Close = close;
+        High = high;
+        Low = low;
+        Volume = volume;
+    }
+}
+
--- TradeAnalyzer/StringFunctions.cs
+++ TradeAnalyzer/StringFunctions.cs
@@ -4,9 +4,56 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TradeAnalyzer
+
+public static class StringFunctions
 {
-    class StringFunctions
+
+    public static string GetClearlyDefinedString(string s)
     {
+        return s.Aggregate("", (current, c) => current + GetClearDefinedStringChar(c));
+    }
+
+    private static string GetClearDefinedStringChar(char c)
+    {
+        string ss = c.ToString().ToUpper();
+        if (string.IsNullOrWhiteSpace(ss))
+        {
+            return " ";
+        }
+        switch (ss)
+        {
+            case "К":
+                return "K";
+            case "Е":
+                return "E";
+            case "Н":
+                return "H";
+            case "З":
+                return "3";
+            case "Х":
+                return "X";
+            case "В":
+                return "B";
+            case "А":
+                return "A";
+            case "Р":
+                return "P";
+            case "О":
+                return "O";
+            case "С":
+                return "C";
+            case "М":
+                return "M";
+            case "Т":
+                return "T";
+            case "1":
+                return "L";
+            case "8":
+                return "S";
+            case "0":
+                return "O";
+        }
+        return ss;
     }
 }
+
